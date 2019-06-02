414d832f1d2923393b766ddd8b183e19f00303b5
Hitesh Dua <hiteshdua1@gmail.com>
2019-06-03 02:01:05 +0530

    Adding bootstrap css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './bootstrap.min.css'
 import MarkdownRenderer from './components/MarkdownRenderer';
 
 class App extends Component {
